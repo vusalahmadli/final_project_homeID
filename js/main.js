cdef663fd8ac7d3531c9529b3dbc35796186e686
vusalahmadli <vusalna@code.edu.az>
2021-05-04 22:23:46 +0200

    homePage done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+"use strict";
+
+// MODAL TOGGLE
+const openButton = document.getElementById("signin");
+const modalContainer = document.getElementById("modal-container");
+const closeButton = document.getElementById("times");
+
+openButton.addEventListener("click", () => {
+  modalContainer.classList.add("show");
+});
+
+closeButton.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
+
 $(".first-slide").owlCarousel({
   loop: false,
   responsive: {
@@ -64,6 +79,114 @@ $(".second").owlCarousel({
     },
     1900: {
       items: 4,
+    },
+  },
+});
+
+$(".third").owlCarousel({
+  loop: false,
+  dots: true,
+  margin: 20,
+  nav: false,
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+    1200: {
+      items: 2,
+    },
+    1400: {
+      items: 4,
+    },
+    1800: {
+      items: 4,
+    },
+    1900: {
+      items: 4,
+    },
+  },
+});
+$(".fourth").owlCarousel({
+  loop: false,
+  dots: true,
+  margin: 50,
+  nav: false,
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+    1200: {
+      items: 2,
+    },
+    1400: {
+      items: 3,
+    },
+    1800: {
+      items: 3,
+    },
+    1900: {
+      items: 3,
+    },
+  },
+});
+$(".fifth-slider").owlCarousel({
+  loop: false,
+  dots: true,
+  margin: 50,
+  nav: false,
+
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+    1200: {
+      items: 2,
+    },
+    1400: {
+      items: 4,
+    },
+    1800: {
+      items: 4,
+    },
+    1900: {
+      items: 4,
+    },
+  },
+});
+$(".six-slider").owlCarousel({
+  loop: false,
+  nav: false,
+  dots: false,
+  margin: 30,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 4,
+    },
+    1000: {
+      items: 6,
     },
   },
 });
